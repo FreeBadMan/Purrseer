f1be01c8f2f28f8b3f12ac83be75c2350bad478b
Vladislav <freeman1813@gmail.com>
2019-04-11 21:33:00 +0300

    Корректно отвечаем на корректные запросы GET и POST

--- povar.js
+++ povar.js
@@ -2,26 +2,33 @@ const http = require('http');
 const qs = require('querystring');
 const MongoClient = require('mongodb').MongoClient;
 
-const mongourl=''
+const mongourl='' //Не забываем почистить перед коммитом
 const client = new MongoClient(mongourl);
+var givenTasks = [];
+givenTasks[1]='https://2ch.hk'
 
+function randInt(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+}
 client.connect(function(err, client) {
 	const server = http.createServer(listener).listen(8001);
 
-	var givenTasks = [];
+
 
 	function listener(req, res) {
-		const db = client.db('purrseer');
+		const db = client.db('purrdb');
 		const tasks = db.collection('tasks');
 		const treasureBox = db.collection('treasureBox');
-
+		console.log('Опа, запросек '+req.method);
 		//распределяем запросы по типу
 		switch (req.method) {
 			case 'GET':
 				getWorker(req, res, tasks);
 				break;
 			case 'POST':
-				postWorker(req, res);
+				postWorker(req, res, tasks, treasureBox);
 				break;
 			default:
 				res.writeHead(404);
@@ -36,41 +43,32 @@ function getWorker(req, res, tasks){
 	tasks.findOneAndUpdate(
 		{busy: false},
 		{$set: {busy: true}}, function(err, r) {
-			let id = r.value._id;
-			let newTask  = r.value;
+			if (r.value!=null){
 
-			givenTasks[id] = newTask;//переносим задание в массив с выданными заданиями
-			res.writeHead(200);//говорим, что все ок
-			res.write(JSON.stringify(newTask));//отправляем id задания и ссылку, на которой надо искать
-			res.end();
+				let id = parseInt(r.value._id);
+				let newTask  = {id:r.value._id,url:r.value.url};
+
+				givenTasks[id] = r.value.url;//переносим задание в массив с выданными заданиями
+				res.writeHead(200);//говорим, что все ок
+				res.write(JSON.stringify(newTask));//отправляем id задания и ссылку, на которой надо искать
+				res.end();
+
+			} else {
+				res.writeHead(418);
+				res.end(); //если доступных заданий(ссылок) нет, то отвечаем, что кофе варить мы не умеем
+			}
 		}
 	);
-	/*
-	if(tasks.length == 0) { //Проверяем что есть задачи
-		res.writeHead(204)//говорим, что все ок но зайти надо позже
-		res.end();
-		return;
-	}
 
-	res.writeHead(200)//говорим, что все ок
-
-	let newTask=tasks.shift(); //Берем новую ссылку из массива заданий
-
-	let id = ""+Date.now()+randInt(1,1234567); //Создаем заданию id
-
-	givenTasks[id]=newTask;//переносим заданиев массив с выданными заданиями //а вот это точно будет жить в базе
-
-	res.write( JSON.stringify({"id":id, "task":newTask}));
-	res.end();*/
 }
 
-function postWorker(req, res){
+function postWorker(req, res, tasks, treasureBox){
 	/*
 	Образец:
 		{
 			"id": 0,
-			"treasures": "",
-			"postcards": "",
+			"treasures": ["",""],
+			"postcards": ["",""],
 			"status": "OK"
 		}
 	 */
@@ -95,20 +93,24 @@ function postWorker(req, res){
 		if ((post.id === undefined) || (post.treasures === undefined) || (post.postcards === undefined) || (post.status === undefined)) {
 			res.writeHead(406);
 			res.end();
-			return ;
+			return ; //проверка на корректность полей запроса
 		}
 
-		if (givenTasks[post.id]) { //проверяем, не левый ли это запрос
+		if (givenTasks[parseInt(post.id)]) { //TODO сделать проверку post.status, сделать проверку на то, что найденную ссылку не находили раньше
 
-			tasks=tasks.concat(post.postcards); //добавляем найденный ссылки в массив с заданиями @TODO добавить проверку поля
+			postcardsToInsert = [];
+			for (i in post.postcards){
+				postcardsToInsert.push( {_id:''+randInt(1,1234567)+Date.now(), url:post.postcards[i], busy:false} )
+			};
+			tasks.insert(postcardsToInsert); //добавляем найденные по ссылке ссылки в коллекцию ссылок
+			
+			treasureBox.findOneAndUpdate( //Добавляем сокровища(найденные по ссылке слова) в коллекцию сокровищ в БД
+				{url:givenTasks[parseInt(post.id)]},
+				{$push: {treasures:{$each:post.treasures} }},
+				{upsert:true}//если записи о словах по данной ссылке нет, добавляем новую запись, иначе добавляем данные в старую
+			)
 
-			if (treasureBox[givenTasks[post.id]]){}
-			else{ treasureBox[givenTasks[post.id]]=''};
-			for (item in post.treasures){
-				treasureBox[givenTasks[post.id]]+=post.treasures[item]
-			}; //Кладем найденные по ссылке слова в объект с полем <ссылка>
-
-			delete givenTasks[post.id];
+			delete givenTasks[parseInt(post.id)];
 			res.writeHead(200);
 			res.write("OK");
 			res.end(); //удаляем задание из ожидаемых и говорим, что все хорошо
